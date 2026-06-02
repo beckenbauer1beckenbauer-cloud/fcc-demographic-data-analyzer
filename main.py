a714aa2deaf5c82482ed344f0642e3ceb603275f
codingpro3 <mafhahouamjmamaham@gmail.com>
2026-06-02 11:47:39 +0100

    Create main.py to execute analysis and tests
    Add main.py to run demographic data analysis and tests

---
+++ main.py
@@ -0,0 +1,8 @@
+import demographic_data_analyzer
+from unittest import main
+
+# Run the analyzer function and print results to console
+demographic_data_analyzer.calculate_demographic_data()
+
+# Run automated tests to verify the solution against freeCodeCamp requirements
+main(module='test_module', exit=False)
